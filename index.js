ab9384048c8bfd960a3a3a52ad08a1e1e4d419ef
kingcody <me@codymize.com>
2014-04-30 04:04:09 -0400

    proper rewrite

--- index.js
+++ index.js
@@ -2,41 +2,43 @@ var fs = require('fs');
 var path = require('path');
 var async = require('async');
 
-var checkFileForModifiedImports = async.memoize(function(filepath, fileCheckCallback) {
-  fs.readFile(filepath, 'utf8', function(error, data) {
-    var directoryPath = path.dirname(filepath);
-    var regex = /@import (?:\([^)]+\) )?"(.+?)(\.less)?"/g
-    var match;
+module.exports = function(details) {
+  var checkFileForModifiedImports = async.memoize(function(filepath, fileCheckCallback) {
+    fs.readFile(filepath, 'utf8', function(error, data) {
+      var directoryPath = path.dirname(filepath);
+      var regex = /@import (?:\([^)]+\) )?["'](.+?)(\.less)?["']/g
+      var match;
 
-    function checkNextImport() {
-      if ((match = regex.exec(data)) === null) {
-        return fileCheckCallback(false); // all @import files has been checked.
-      }
-
-      var importFilePath = path.join(directoryPath, match[1] + '.less');
-      fs.exists(importFilePath, function(exists) {
-        if (!exists) { // @import file does not exists.
-          return checkNextImport(); // skip to next
+      function checkNextImport() {
+        if ((match = regex.exec(data)) === null) {
+          return fileCheckCallback(false); // all @import files has been checked.
         }
 
-        fs.stat(importFilePath, function(error, stats) {
-          if (stats.mtime > details.time) { // @import file has been modified, -> include it.
-            fileCheckCallback(true);
-          } else { // @import file has not been modified but, lets check the @import's of this file.
-            checkFileForModifiedImports(importFilePath, function(hasModifiedImport) {
-              if (hasModifiedImport) {
-                fileCheckCallback(true);
-              } else {
-                checkNextImport();
-              }
-            });
+        var importFilePath = path.join(directoryPath, match[1] + '.less');
+        fs.exists(importFilePath, function(exists) {
+          if (!exists) { // @import file does not exists.
+            return checkNextImport(); // skip to next
           }
+
+          fs.stat(importFilePath, function(error, stats) {
+            if (stats.mtime > details.time) { // @import file has been modified, -> include it.
+              fileCheckCallback(true);
+            } else { // @import file has not been modified but, lets check the @import's of this file.
+              checkFileForModifiedImports(importFilePath, function(hasModifiedImport) {
+                if (hasModifiedImport) {
+                  fileCheckCallback(true);
+                } else {
+                  checkNextImport();
+                }
+              });
+            }
+          });
         });
-      });
-    };
+      };
 
-    checkNextImport();
+      checkNextImport();
+    });
   });
-});
 
-module.exports = checkFileForModifiedImports;
+  return checkFileForModifiedImports;
+};
